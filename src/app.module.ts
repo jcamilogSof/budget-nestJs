bc7b54357822a7eb01d32304ded016f0e46f3b93
Juan Guerrero Gonzalez <juan.guerrero@sofka.com.co>
2024-01-20 16:48:11 -0500

    Initial structure is created for endpoint routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { IncomeModule } from './income/income.module';
+import { BillsModule } from './bills/bills.module';
+import { SavingsModule } from './savings/savings.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, IncomeModule, BillsModule, SavingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
